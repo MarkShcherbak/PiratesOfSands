d252ac4b28eb37a55d93fcee5fdcb62641104e5b
Shmareck <mark.shcherbak@gmail.com>
2020-05-10 22:56:48 +0300

    TestCommitMark

---
+++ Assets/TestMark/TestMarkScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMarkScript : MonoBehaviour
+{
+    public GameObject gameobj;
+    void Start()
+    {
+        gameobj.SetActive(false);
+    }
+
+  
+}
