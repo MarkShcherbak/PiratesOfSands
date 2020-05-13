05c09950bd9a9b1c09b6096ddcd581e346e81b3e
Nikita Vilisov <invadermd@gmail.com>
2020-05-13 20:26:37 +0700

    Created Canvas and Main - app entry point

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// скрипт висит на пустом объекте Main
+public class Main : MonoBehaviour
+{
+    public Canvas canvas;
+
+    // точка входа в приложение
+    void Start()
+    {
+        CreateMainMenu(canvas);
+
+        // подписываемся на кнопки из меню
+    }
+
+    private void CreateMainMenu(Canvas canvas)
+    {
+        // создаем меню из фабрики UI
+    }
+
+    private void HandleGameStarted()
+    {
+        //создаем игру
+    }
+}
