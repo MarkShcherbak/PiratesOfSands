530736f136ef1caea3d97a9bf75d1c2e1a68c047
Nikita Vilisov <57738727+vilisix@users.noreply.github.com>
2020-05-14 17:43:37 +0700

    Added shipController and shipModelView(not full func yet)

---
+++ Assets/Scripts/Ship/ShipController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+// контроллер корабля
+    public class ShipController
+    {
+        private readonly ShipModelView shipMV;
+        
+
+        public ShipController(ShipModelView shipModelView)
+        {
+            shipMV = shipModelView;
+        }
+    }
---
+++ Assets/Scripts/Ship/ShipModelView.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+    public class ShipModelView : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody rb;
+       // [SerializeField] private CannonModelView[] cannons; TODO add cannons
+    }
