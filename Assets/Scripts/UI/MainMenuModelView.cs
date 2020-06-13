bd810435785a4233d289f15ef42c4a7f77d1b270
Nikita Vilisov <57738727+vilisix@users.noreply.github.com>
2020-06-13 17:03:15 +0700

    Added laps/ships variation to main menu

--- Assets/Scripts/UI/MainMenuModelView.cs
+++ Assets/Scripts/UI/MainMenuModelView.cs
@@ -10,9 +10,17 @@ using UnityEngine.UI;
         public Button startButton;
         public Button exitButton;
 
+        public Text lapsCountText;
+        public Text lapsCountOutlineText;
+
+        public Text shipsCountText;
+        public Text shipsCountOutlineText;
+
         public event EventHandler OnStart = (sender, e) => { };
         public event EventHandler OnExit = (sender, e) => { };
-        
+
+        private int lapsCount = 1;
+        private int shipsCount = 5;
         private void Start()
         {
             startButton.onClick.AddListener(delegate
@@ -23,5 +31,41 @@ using UnityEngine.UI;
             {
                 OnExit(this, EventArgs.Empty);
             });
+        }
+
+        public void IncreaseLapCount()
+        {
+            if(lapsCount < 50)
+            lapsCount++;
+
+            lapsCountText.text = lapsCount.ToString();
+            lapsCountOutlineText.text = lapsCount.ToString();
+        }
+        
+        public void DecreaseLapCount()
+        {
+            if(lapsCount > 1)
+                lapsCount--;
+
+            lapsCountText.text = lapsCount.ToString();
+            lapsCountOutlineText.text = lapsCount.ToString();
+        }
+        
+        public void IncreaseShipCount()
+        {
+            if(shipsCount < 20)
+                shipsCount++;
+
+            shipsCountText.text = shipsCount.ToString();
+            shipsCountOutlineText.text = shipsCount.ToString();
+        }
+        
+        public void DecreaseShipCount()
+        {
+            if(shipsCount > 1)
+                shipsCount--;
+
+            shipsCountText.text = shipsCount.ToString();
+            shipsCountOutlineText.text = shipsCount.ToString();
         }
     }
