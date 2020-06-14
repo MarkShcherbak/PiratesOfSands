fc5910efec3ccbf985eb3126c2d4228d22ca3513
Nikita Vilisov <57738727+vilisix@users.noreply.github.com>
2020-06-14 22:27:53 +0700

    Final arrow layer rotation fix

--- Assets/Scripts/UI/HUD/DirectionArrowModelView.cs
+++ Assets/Scripts/UI/HUD/DirectionArrowModelView.cs
@@ -16,7 +16,8 @@ using UnityEngine;
                 Vector3 direction = CheckpointDirection.position - ShipDirection.position;
                 //float arrowAngle = Vector3.SignedAngle(ShipDirection.forward, direction, Vector3.up);
                 float arrowAngle = AngleSigned(ShipDirection.forward, direction, Vector3.up);
-                this.transform.rotation = Quaternion.Euler(0, 0, -arrowAngle);
+                ArrowTransform.rotation = Quaternion.Euler(0, 0, -arrowAngle);
+				ArrowOutlineTransform.rotation = Quaternion.Euler(0, 0, -arrowAngle);
             }
         }
         
